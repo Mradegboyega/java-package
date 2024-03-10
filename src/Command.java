7085b16d9eeaa4b0c24e78bb72fb7237c10c815d
Adegboyega Ademola <a.adegboyega11@gmail.com>
2024-03-10 12:31:57 +0100

    java course

---
+++ src/Command.java
@@ -0,0 +1,5 @@
+public class Command {
+    public static void main(String[] args) {
+        System.out.println("Learning the basics of java yet again so I can build something complex.");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Learnt java is strongly typed, now I want to dive deep into that world of strongly typed language.");
+    }
+}
